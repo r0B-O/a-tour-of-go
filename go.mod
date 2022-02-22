def794ce827bdae45b334e9792a6564f1ae502e5
r0B-O <neo@r0B-O.dev>
2022-02-22 12:57:03 +0530

    Init - A Tour of Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/r0B-O/a-tour-of-go
+
+go 1.17
---
+++ packages.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Here is a random number", rand.Intn(10))
+}
